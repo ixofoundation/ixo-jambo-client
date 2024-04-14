7a4cd703bc7afc5815e72fda28274d4c13eed39b
Michael Pretorius <michaelpretorius96@gmail.com>
2024-04-14 10:20:10 +0200

    chore: add new swap pool

--- constants/pools.ts
+++ constants/pools.ts
@@ -12,4 +12,4 @@ tokens.set('uixo', { type: TokenType.Native });
 export const pools = new Dictionary((keyObject: Pool) => JSON.stringify(keyObject));
 // Example
 // pools.set({ token1155: 'CARBON', token2: 'USDT' }, 'ixo15d2tyq2jzxmpg32y3am3w62dts32qgzmds9qnr6c87r0gwwr7ynq28958z');
-pools.set({ token1155: 'CARBON', token2: 'uixo' }, 'ixo1d3gupctdquscekqt48g2xnmfnweaqx7hh4l83vcgy5nw7sphjlvqju0vch');
+pools.set({ token1155: 'CARBON', token2: 'uixo' }, 'ixo1zl692z26krvh05nk8vlp5dlkcqjzamqwadxz05ykx6mzqzt44dqsev48vk');
